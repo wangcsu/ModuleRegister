5c5344638b1465f96dd68c9f2a44f4cc6fc98873
Ge Wang <wk.qwerty17@gmail.com>
2017-07-19 21:14:36 -0700

    Add update.php page, and show remaining seats functionality

--- update.php
+++ update.php
@@ -1,9 +1,10 @@
 <?php
 require 'config.php';
 require 'db.php';
-// Error message variables
+
 $msg = '';
 $msgClass = '';
+
 $query = "SELECT COUNT(*) AS 'count' FROM students WHERE time_slot = 'Wednesday, July 5th, 2:00pm - 4:00pm'";
 $result = mysqli_query($conn, $query);
 $row = mysqli_fetch_assoc($result);
@@ -24,74 +25,89 @@ $result = mysqli_query($conn, $query);
 $row = mysqli_fetch_assoc($result);
 $count = $row['count'];
 $seatsRemaining4 = 8 - $count;
+
+// Get sid
+$sid = mysqli_real_escape_string($conn, $_GET['sid']);
 // Check for submit
 if (filter_has_var(INPUT_POST, 'submit')){
-    // Get data from the form
-    $sid = mysqli_real_escape_string($conn, $_POST['sid']);
+    $sid_new = mysqli_real_escape_string($conn, $_POST['sid']);
     $firstName = mysqli_real_escape_string($conn, $_POST['first_name']);
     $lastName = mysqli_real_escape_string($conn, $_POST['last_name']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $timeSlot = mysqli_real_escape_string($conn, $_POST['time_slot']);
-    $timeSlot = substr($timeSlot, 0, strpos($timeSlot, "-") + 8);
-    // Check required field
-    if (!empty($sid) && !empty($firstName) && !empty($lastName) && !empty($email) && !empty($timeSlot)){
-        // Check if email is valid
+    $timeSlot = substr($timeSlot, 0, strpos($timeSlot, '-') + 8);
+    // Check for required fields
+    if (!empty($sid_new) && !empty($firstName) && !empty($lastName) && !empty($email) && !empty($timeSlot)) {
+        // Check for valid email address
         if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
             $msg = 'Please enter a valid email address';
             $msgClass = 'alert-danger';
         } else {
             // Check if there are seats available
             if (($timeSlot == 'Wednesday, July 5th, 2:00pm - 4:00pm' && $seatsRemaining1 <= 0)
-                    || ($timeSlot == 'Wednesday, July 19th, 2:00pm - 4:00pm' && $seatsRemaining2 <= 0)
-                    || ($timeSlot == 'Wednesday, August 2nd, 2:00pm - 4:00pm' && $seatsRemaining3 <= 0)
-                    || ($timeSlot == 'Wednesday, August 16th, 2:00pm - 4:00pm' && $seatsRemaining4 <= 0)) {
+                || ($timeSlot == 'Wednesday, July 19th, 2:00pm - 4:00pm' && $seatsRemaining2 <= 0)
+                || ($timeSlot == 'Wednesday, August 2nd, 2:00pm - 4:00pm' && $seatsRemaining3 <= 0)
+                || ($timeSlot == 'Wednesday, August 16th, 2:00pm - 4:00pm' && $seatsRemaining4 <= 0)) {
                 $msg = 'No available seat remaining, please choose a different time slot.';
                 $msgClass = 'alert-danger';
             } else {
-                // Check if the student already registered
-                $query = "SELECT COUNT(*) AS 'count' FROM students WHERE SID = '$sid' AND email = '$email' AND first_name = '$firstName' AND last_name = '$lastName'";
+                // Increase the available seats
+                $query = "SELECT time_slot FROM students WHERE SID = '$sid'";
+                $result = mysqli_query($conn, $query);
+                $row = mysqli_fetch_assoc($result);
+                $time_slot = $row['time_slot'];
+                switch ($time_slot) {
+                    case 'Wednesday, July 5th, 2:00pm - 4:00pm':
+                        $seatsRemaining1 += 1;
+                        break;
+                    case 'Wednesday, July 19th, 2:00pm - 4:00pm':
+                        $seatsRemaining2 += 1;
+                        break;
+                    case 'Wednesday, August 2nd, 2:00pm - 4:00pm':
+                        $seatsRemaining3 += 1;
+                        break;
+                    case 'Wednesday, August 16th, 2:00pm - 4:00pm':
+                        $seatsRemaining4 += 1;
+                        break;
+                    default:
+                        ;
+                }
+                // Delete the old record
+                $query = "DELETE FROM students WHERE SID = '$sid'";
+                mysqli_query($conn, $query);
+                // Check if the new SID or email is in use
+                $query = "SELECT COUNT(*) AS 'count' FROM students WHERE SID = '$sid_new' OR email = '$email'";
                 $result = mysqli_query($conn, $query);
                 $row = mysqli_fetch_assoc($result);
                 $count = $row['count'];
-                if ($count > 0){
-                    $url = ROOT_URL.'update.php?sid='.$sid;
-                    header('Location: '.$url.'');
+                if ($count > 0) {
+                    $msg = 'SID: ' . $sid . ' or Email: ' . $email . ' has already been registered, please use a different one';
+                    $msgClass = 'alert-warning';
                 } else {
-                    // Check if the email or SID is already in use
-                    $query = "SELECT COUNT(*) AS 'count' FROM students WHERE SID = '$sid' OR email = '$email'";
-                    $result = mysqli_query($conn, $query);
-                    $row = mysqli_fetch_assoc($result);
-                    $count = $row['count'];
-                    if ($count > 0) {
-                        $msg = 'SID: ' . $sid . ' or Email: ' . $email . ' has already been registered, please use a different one';
-                        $msgClass = 'alert-warning';
-                    } else {
-                        // Insert into the database
-                        $query = "INSERT INTO students(SID, first_name, last_name, email, time_slot) VALUES('$sid', '$firstName', '$lastName', '$email', '$timeSlot')";
-                        if (mysqli_query($conn, $query)) {
-                            $msg = 'You registered at ' . $timeSlot . ' successfully.';
-                            $msgClass = 'alert-success';
-                            // Decrease the available seats
-                            switch ($timeSlot) {
-                                case 'Wednesday, July 5th, 2:00pm - 4:00pm':
-                                    $seatsRemaining1 -= 1;
-                                    break;
-                                case 'Wednesday, July 19th, 2:00pm - 4:00pm':
-                                    $seatsRemaining2 -= 1;
-                                    break;
-                                case 'Wednesday, August 2nd, 2:00pm - 4:00pm':
-                                    $seatsRemaining3 -= 1;
-                                    break;
-                                case 'Wednesday, August 16th, 2:00pm - 4:00pm':
-                                    $seatsRemaining4 -= 1;
-                                    break;
-                                default:
-                                    ;
-                            }
-                        } else {
-                            $msg = 'ERROR: ' . mysqli_error($conn);
-                            $msgClass = 'alert-danger';
+                    // Insert the new record into database
+                    $query = "INSERT INTO students(SID, first_name, last_name, email, time_slot) VALUES('$sid_new', '$firstName', '$lastName', '$email', '$timeSlot')";
+                    if (mysqli_query($conn, $query)) {
+                        $msg = 'You registered at ' . $timeSlot . ' successfully.';
+                        $msgClass = 'alert-success';
+                        switch ($timeSlot) {
+                            case 'Wednesday, July 5th, 2:00pm - 4:00pm':
+                                $seatsRemaining1 -= 1;
+                                break;
+                            case 'Wednesday, July 19th, 2:00pm - 4:00pm':
+                                $seatsRemaining2 -= 1;
+                                break;
+                            case 'Wednesday, August 2nd, 2:00pm - 4:00pm':
+                                $seatsRemaining3 -= 1;
+                                break;
+                            case 'Wednesday, August 16th, 2:00pm - 4:00pm':
+                                $seatsRemaining4 -= 1;
+                                break;
+                            default:
+                                ;
                         }
+                    } else {
+                        $msg = 'ERROR: ' . mysqli_error($conn);
+                        $msgClass = 'alert-danger';
                     }
                 }
             }
@@ -100,12 +116,12 @@ if (filter_has_var(INPUT_POST, 'submit')){
         $msg = 'Please fill in all fields';
         $msgClass = 'alert-danger';
     }
-
 }
 ?>
+
 <html>
 <head>
-    <title>Register</title>
+    <title>Update</title>
     <link rel="stylesheet" href="https://bootswatch.com/paper/bootstrap.min.css">
 </head>
 <body>
@@ -140,7 +156,16 @@ if (filter_has_var(INPUT_POST, 'submit')){
             <li>Any problem? Send a message to <a href="#">Administrator</a></li>
         </ul>
     </div>
-    <?php if ($msg !=''): ?>
+    <?php if ($msg == ''): ?>
+    <div class="alert alert-dismissible alert-warning">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <h4>Warning!</h4>
+        <P>You've already registered!</P>
+        <p>If you wish to update your information, please fill out the form below.</p>
+        <p>Or,</p>
+        <p><a href="<?php echo ROOT_URL; ?>">Back to registration</a></p>
+    </div>
+    <?php else: ?>
         <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
     <?php endif; ?>
     <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>" class="form-horizontal">
@@ -183,7 +208,7 @@ if (filter_has_var(INPUT_POST, 'submit')){
             <div class="form-group">
                 <div class="col-lg-10 col-lg-offset-2">
                     <button type="reset" class="btn btn-default">Cancel</button>
-                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" name="submit" class="btn btn-primary">Update</button>
                 </div>
             </div>
         </fieldset>
@@ -193,3 +218,4 @@ if (filter_has_var(INPUT_POST, 'submit')){
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
+
