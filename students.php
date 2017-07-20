244f263aaacc0ef514aa06539c661ed0dcd3992d
Ge Wang <wk.qwerty17@gmail.com>
2017-07-19 21:47:41 -0700

    Display all registered students on the lecturer page

--- students.php
+++ students.php
@@ -1,7 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ge Wang
- * Date: 7/19/2017
- * Time: 9:21 PM
- */
+require 'config.php';
+require 'db.php';
+
+$number = 1;
+
+// Create query
+$query = 'SELECT * FROM students ORDER BY time_slot DESC, last_name, first_name';
+
+// Get result
+$result = mysqli_query($conn, $query);
+
+// Fetch data
+$students = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// Free result
+mysqli_free_result($result);
+
+// Close connection
+mysqli_close($conn);
+?>
+<?php include('header.php'); ?>
+<div class="container">
+    <h3>Module Demonstration Schedule</h3>
+    <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>SID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Time Slot</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($students as $student): ?>
+            <tr>
+                <td><?php echo $number++; ?></td>
+                <td><?php echo $student['SID']; ?></td>
+                <td><?php echo $student['first_name'].' '.$student['last_name']; ?></td>
+                <td><?php echo $student['email']; ?></td>
+                <td><?php echo $student['time_slot']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<?php include('footer.php'); ?>
+
